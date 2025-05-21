e72a1bb50cce26c07bf71c4c742f78e60da80fd7
Daria Deriagina <w0862447@myscc.ca>
2025-05-21 14:48:20 -0400

    Services section is added

--- script.js
+++ script.js
@@ -30,3 +30,22 @@ window.addEventListener("scroll", () => {
 		}
 	});
 });
+
+// Scroll Animation Script
+
+const observer = new IntersectionObserver(
+	(entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add("show");
+			}
+		});
+	},
+	{
+		threshold: 0.1,
+	}
+);
+
+document
+	.querySelectorAll(".animate-on-scroll")
+	.forEach((el) => observer.observe(el));
