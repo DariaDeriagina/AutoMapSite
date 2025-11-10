842921b980b398d9d7b132c234f16b1c64089cca
Daria Deriagina <w0862447@myscc.ca>
2025-11-10 18:14:16 -0500

    Fix Firebase initialization and logbook connection

--- firebase.js
+++ firebase.js
@@ -1,54 +1,62 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+// firebase.js (ES module)
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
 import {
 	getFirestore,
 	collection,
-	addDoc,
-	getDocs,
-	deleteDoc,
 	doc,
-	onSnapshot,
-	query,
-	orderBy,
+	addDoc,
 	updateDoc,
+	deleteDoc,
 	writeBatch,
 	serverTimestamp,
-} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+	query,
+	orderBy,
+	onSnapshot,
+} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
 import {
 	getAuth,
-	signInAnonymously,
 	onAuthStateChanged,
-} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+	signInAnonymously,
+} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
 
+// TODO: вставь свой конфиг
 const firebaseConfig = {
-	apiKey: "AIzaSyCzPTq388hwmATTMhEXjSR9naAnI2xRxQw",
-	authDomain: "automap-logbook.firebaseapp.com",
-	projectId: "automap-logbook",
-	storageBucket: "automap-logbook.firebasestorage.app",
-	messagingSenderId: "569545953515",
-	appId: "1:569545953515:web:6f32e5e5e1385f27e6eeae",
+	apiKey: "YOUR_WEB_API_KEY",
+	authDomain: "YOUR_PROJECT.firebaseapp.com",
+	projectId: "YOUR_PROJECT_ID",
+	storageBucket: "YOUR_PROJECT.appspot.com",
+	messagingSenderId: "XXXX",
+	appId: "1:XXXX:web:XXXX",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-// anonymous auth
-signInAnonymously(auth).catch(console.error);
-
-// expose for other scripts
+// Экспортируем в window именно то, что использует logbook.js
+window.firebaseApp = app;
 window.firebaseDB = db;
 window.firebaseAuth = auth;
+
+// Собираем helper-объект fs, чтобы logbook.js мог вызывать как сейчас
 window.fs = {
 	collection,
-	addDoc,
-	getDocs,
-	deleteDoc,
 	doc,
-	onSnapshot,
-	query,
-	orderBy,
+	addDoc,
 	updateDoc,
+	deleteDoc,
 	writeBatch,
 	serverTimestamp,
+	query,
+	orderBy,
+	onSnapshot,
 	onAuthStateChanged,
 };
+
+// Стартуем анонимную авторизацию, чтобы onAuthStateChanged в logbook.js сработал
+signInAnonymously(auth).catch((err) => {
+	console.error("[firebase] anonymous sign-in failed:", err);
+});
+
+console.log("[firebase] init ok", { hasDB: !!window.firebaseDB });
